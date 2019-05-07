b16e32cfd95fe2f578ec0f8153f3b5035f204f6d
mohamed fathy <m.fathy7793@gmail.com>
2019-05-08 00:14:09 +0200

    iljklj

---
+++ sample/src/main/java/com/yarolegovich/slidingrootnav/sample/Activity/Profile_page.java
@@ -0,0 +1,15 @@
+package com.yarolegovich.slidingrootnav.sample.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.yarolegovich.slidingrootnav.sample.R;
+
+public class Profile_page extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile_page);
+    }
+}
